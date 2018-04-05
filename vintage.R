2c22db5ce48f0102f9f4425e8281d1245b14271a
Unknown <anett0902@hotmail.com>
2018-04-05 23:38:18 +0200

    added date axis, added means

--- vintage.R
+++ vintage.R
@@ -9,12 +9,22 @@ MyData <- read.csv(file="vintage_data.csv", header=TRUE, sep=",")
 # b. L mátrix elõállítása a betöltött adat 2-7. oszlopaiból 
 
 mymatrix <- as.matrix(MyData)
-
+# mátrix létrehozása a c. feladathoz, segítség a plotoláshoz, az indexelés miatt
 mymatrix[,-1]
 
 # c. Különbözõ idõpontban induló állományok idõbeli alakulása
 
-matplot(mymatrix[,-1], type="b", pch=15:20,col=c(1:6))
+matplot(mymatrix[,-1], type="b", pch=15:20,col=c(1:6),axes=FALSE)
 
+axis(2)
+axis(1,1:45,mymatrix[,1])
 names <-matrix(c( "0-3 months", "3-6 months", "6-9 months", "9-12 months" , "12-15 months", "15-18 month"), 6,1)
+
 legend("bottomleft", inset= 0.01, legend=names, col=c(1:6), pch=15:20, bty="n", horiz=F)
+
+# d.  az átlagos állomány méretét az egyes kohorszok esetén
+mymatrix[,2]
+for (i in 2:7){
+  print (mean(as.numeric(mymatrix[,i]), na.rm=TRUE))
+}
+
